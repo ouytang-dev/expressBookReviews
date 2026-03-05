b962315eb76962f47cc19e8bc86c79c89691988f
ouytang-dev <ouytang.dev@gmail.com>
2026-03-05 17:31:39 +1100

    task1 complete corrected

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -18,9 +18,10 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-    const isbn = req.params.isbn;
-    res.send(books[isbn]);
- });
+//    res.send(books[req.params.isbn]);
+  return res.status(300).json({message: "Yet to be implemented"});
+
+  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
